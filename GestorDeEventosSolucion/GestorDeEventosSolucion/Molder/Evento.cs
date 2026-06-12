4f223267e4bce971fb5c788c4fe528ef0f42b53b
dragonikalfa <alfathehybrid202181@gmail.com>
2026-06-11 20:41:32 -0400

    Clase evento
    ya cree la clase evento pero no se porque no se modifico el nombre en su tiempo y se nota lo estupido que soy o mi autismo esta activado

--- GestorDeEventosSolucion/GestorDeEventosSolucion/Molder/Evento.cs
+++ GestorDeEventosSolucion/GestorDeEventosSolucion/Molder/Evento.cs
@@ -1,6 +1,14 @@
 ﻿namespace GestorDeEventosSolucion.Molder
 {
-    public class Class
+    public class Evento
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime Scheduling { get; set; }
+        public int EspacioId { get; set; }
+        public int ResponsableId { get; set; }
+
+       
     }
 }
